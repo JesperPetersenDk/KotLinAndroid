3d25107d9cc4dc73bc6842b6a01947f7b5a1724b
Jesper Petersen <info@ordklar.dk>
2019-12-03 11:45:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kotlincph/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlincph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val buttons = mutableListOf<Button>()
+
+    fun findAllButton(parent:ViewGroup)
+    {
+        for (i in 0 until parent.childCount) {
+            val child = parent.getChildAt(i)
+
+            if (child is Button && !child.text.toString().contains("=", false)){
+                buttons.add(child)
+            }
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findAllButton(findViewById<ViewGroup>(R.id.root))
+
+        for (button in buttons)
+        {
+            button.setOnClickListener(
+                textView.setText(textView.text.toString()+button.text.toString())
+            )
+        }
+    }
+}
